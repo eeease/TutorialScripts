92724a0a4a980fdb1779c5bc227503fb6518b0c4
eeease <ericmg123@gmail.com>
2020-11-25 05:35:41 -0500

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Here, we declare variables.
+    public GameObject objToSpawn;
+    //public means the var is exposed in the inspector, which is super helpful.
+    public float timeLeft, originalTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tick down our timer:
+        timeLeft -= Time.deltaTime;
+        //timeLeft = timeLeft - Time.deltaTime;
+        if(timeLeft<=0)
+        {
+            SpawnIt();
+
+            //reset the time:
+            timeLeft = originalTime;
+        }
+
+        //let's also spawn on button press:
+        if(Input.GetButtonDown("Jump"))
+        {
+            SpawnIt();
+        }
+    }
+
+    void SpawnIt()
+    {
+        //spawn our coin:
+        Instantiate(objToSpawn, transform.position, Quaternion.identity);
+    }
+}
